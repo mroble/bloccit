51478dd0e337bdf2410a4f30f461580d7e9da382
Maureen Roble <moroble@gmail.com>
2015-05-27 07:10:12 -0400

    Add advertisement controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Bloccit::Application.routes.draw do
+
   resources :posts
 
   get 'about' => 'welcome#about'
+  resources :advertisements
 
+  get "welcome/index"
+  get "welcome/about"
+  get "welcome/contact"
 
-    root to: 'welcome#index'
+  root to: 'welcome#index'
 
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517025441) do
+ActiveRecord::Schema.define(version: 20150527100728) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "advertisings", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
