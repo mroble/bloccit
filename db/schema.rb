3ade5a6b7f0b9f09cbeacbd0020d5b6d3fa54656
Maureen Roble <moroble@gmail.com>
2015-06-04 21:25:53 -0400

    Add user and post association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531204508) do
+ActiveRecord::Schema.define(version: 20150605005742) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -44,7 +44,10 @@ ActiveRecord::Schema.define(version: 20150531204508) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,33 @@
 require 'faker'
 
+ # Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+ 
+ # Note: by calling `User.new` instead of `create`,
+ # we create an instance of User which isn't immediately saved to the database.
+ 
+ # The `skip_confirmation!` method sets the `confirmed_at` attribute
+ # to avoid triggering an confirmation email when the User is saved.
+ 
+ # The `save` method then saves this User to the database.
+
+
 unique_post="This is a unique post"
 unique_comment="This is a unique comment on a random post"
  
  # Create Posts
  50.times do
    Post.create!(
+     user:   users.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -25,6 +47,7 @@ posts = Post.all
 
 100.times do
    Comment.create!(
+       # user: users.sample,   # we have not yet associated Users with Comments
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
@@ -57,6 +80,16 @@ Question.create!(
   end
 
 questions = Question.all
+
+
+
+ user = User.first
+ user.skip_reconfirmation!
+ user.update_attributes!(
+   email: 'moroble@gmail.com',
+   password: 'helloworld'
+ )
+
 
 
 
