3062bc23e3d4b1c4d7b700d4969762ea7c4e4471
Maureen Roble <moroble@gmail.com>
2015-06-07 16:41:20 -0400

    Add policy scope stuff

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,20 @@
 class PostPolicy < ApplicationPolicy
+   class Scope < Scope 
+    def resolve 
+      if user.admin? || user.moderator? 
+      scope.all
+
+      else
+      scope.where(:id => user.id).exists?
+      end
+    end
+  end 
+
+
   def index?
-    true
+    user.admin? || user.id?
   end
- end
+
+end
+
+
