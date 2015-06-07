2eb4a9352f4cbec419ec8e6e1fbab8957ab06925
Maureen Roble <moroble@gmail.com>
2015-06-07 07:57:51 -0400

    Add new scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
-
-
+  
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order('posts.title ASC') }
+  scope :ordered_by_reverse_created_at, -> { order('posts.created_at DESC') }
+
 end
