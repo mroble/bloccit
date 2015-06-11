ca908641f4169a00964ba84630ad985257540216
Maureen Roble <moroble@gmail.com>
2015-06-11 13:45:00 -0400

    Add topic model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607120524) do
+ActiveRecord::Schema.define(version: 20150611174327) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20150607120524) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
