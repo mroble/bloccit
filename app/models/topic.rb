211f13d4e78ffe993a9fbd1a3e4b9a7d4e7fcecc
Maureen Roble <moroble@gmail.com>
2015-06-18 11:09:18 -0400

    Debug topic model error

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   has_many :posts
 
 
-  validates :title, length: { minimum: 5 }, presence: true
+  validates :name, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
