750cae926e0dc01039e7c9b330e664e179a97dec
Maureen Roble <moroble@gmail.com>
2015-06-18 11:00:28 -0400

    Add form partial for topics model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts
+
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 end
