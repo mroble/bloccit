a07e08c5d952de78aeda91c29a4a00e9acb1f038
Maureen Roble <moroble@gmail.com>
2015-06-19 10:21:04 -0400

    Refactor topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,7 @@ class Topic < ActiveRecord::Base
 
 
   validates :name, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 20 }, presence: true
+  validates :description, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
 end
